6493691a4c1d4175f94c6aaf53ddba85c13c6f4c
KmXK <guydo.k@yandex.by>
2024-01-22 17:02:04 +0300

    Removed legacy data

--- CF.Web/ClientApp/src/app/canvas/models/canvas-mouse-position.model.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Point } from "../../shared/models/point.model";
-
-export class CanvasMousePosition {
-    constructor(
-        public local: Point,
-        public global: Point
-    ) {
-    }
-}
